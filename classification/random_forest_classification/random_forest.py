f2b33997dff273714b42fb08eeb130d11520794a
Nishadh Aluthge <nishadh.aluthge@sky.uk>
2020-04-11 12:32:20 +0100

    Restructured comments on Random Forest classifier

--- classification/random_forest_classification/random_forest.py
+++ classification/random_forest_classification/random_forest.py
@@ -25,7 +25,7 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# Fitting logistic regression to train dataset
+# Fitting Random Forest classifier to train dataset
 classifier = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0)
 classifier.fit(X_train, y_train)
 
