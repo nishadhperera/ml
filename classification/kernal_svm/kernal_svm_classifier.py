3cfae48a996759888a7955ad7f8f3749df9ddb7e
Nishadh Aluthge <nishadh.aluthge@sky.uk>
2020-04-11 12:28:48 +0100

    Trained the kernal SVM and visualize the results

--- classification/kernal_svm/kernal_svm_classifier.py
+++ classification/kernal_svm/kernal_svm_classifier.py
@@ -25,47 +25,49 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# Fitting logistic regression to train dataset
+# Create a Kernal with one option out of ['poly', 'linear', 'sigmoid', 'rbf']
 classifier = SVC(kernel='rbf', random_state=0)
+# Fitting Kernal SVM classifier to train dataset
+classifier.fit(X_train, y_train)
 
-# # Predict test set results
-# y_pred = classifier.predict(X_test)
-#
-# # Making the confusion matrix
-# cm = confusion_matrix(y_test, y_pred)
-#
-# # Visualizing the training set results
-# X_set, y_set = X_train, y_train
-# X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
-#                      np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01)
-#                      )
-# plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-#              alpha=0.75, cmap=ListedColormap(('red', 'green')))
-# plt.xlim(X1.min(), X1.max())
-# plt.ylim(X2.min(), X2.max())
-# for i, j in enumerate(np.unique(y_set)):
-#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('red', 'green'))(i), label=j)
-# plt.title('XXX Classification (Training Set)')
-# plt.xlabel('Age')
-# plt.ylabel('Estimated Salary')
-# plt.legend()
-# plt.show()
-#
-# # Visualizing the test set results
-# X_set, y_set = X_test, y_test
-# X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
-#                      np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01)
-#                      )
-# plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-#              alpha=0.75, cmap=ListedColormap(('red', 'green')))
-# plt.xlim(X1.min(), X1.max())
-# plt.ylim(X2.min(), X2.max())
-# for i, j in enumerate(np.unique(y_set)):
-#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('red', 'green'))(i), label=j)
-# plt.title('XXX Classification (Test Set)')
-# plt.xlabel('Age')
-# plt.ylabel('Estimated Salary')
-# plt.legend()
-# plt.show()
-#
-# print('Complete')
+# Predict test set results
+y_pred = classifier.predict(X_test)
+
+# Making the confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Visualizing the training set results
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01)
+                     )
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('red', 'green'))(i), label=j)
+plt.title('Kernal SVM Classification (Training Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualizing the test set results
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01)
+                     )
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('red', 'green'))(i), label=j)
+plt.title('Kernal SVM Classification (Test Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+print('Complete')
